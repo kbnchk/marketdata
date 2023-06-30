fb38c68828efbac5ba98704342a9ef019a472bb6
kbnchk <kabanchik136@gmail.com>
2023-06-30 15:02:37 +0300

    history basic test

--- marketdata_test.go
+++ marketdata_test.go
@@ -3,13 +3,26 @@ package marketdata
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 // TODO!
 
-func TestGetData(t *testing.T) {
+func TestGetDOM(t *testing.T) {
 	source := GarantexNew()
 	got, err := source.GetDOM(USDTRUB)
+	if err != nil {
+		t.Errorf("error getting data = %v", err)
+		return
+	}
+	if reflect.ValueOf(got).IsZero() {
+		t.Error("returned empty data")
+	}
+}
+
+func TestGetHistory(t *testing.T) {
+	source := GarantexNew()
+	got, err := source.GetHistoryToDate(USDTRUB, time.Now().Add(-10*time.Minute))
 	if err != nil {
 		t.Errorf("error getting data = %v", err)
 		return
